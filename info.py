0edf16dd2a81f08885cb093506e88554fac40dc6
mephisto-of-hell <125276079+mephisto-of-hell@users.noreply.github.com>
2023-04-06 02:33:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ class evamaria(Client):
         )
 
 # Bot information
-SESSION = environ.get('SESSION", "bot")
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
