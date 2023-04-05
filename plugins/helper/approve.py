46174f879a2a55f12ca8cfb8cc54d18e465f87a4
mephisto-of-hell <125276079+mephisto-of-hell@users.noreply.github.com>
2023-04-06 01:58:47 +0530

    asdd

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -2,7 +2,9 @@ import os
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
-from info import CHAT_ID, TEXT, APPROVED 
+from info import CHAT_ID, TEXT, APPROVED, LOG_CHANNEL
+from Script import script
+from database.users_chats_db import db
 
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
@@ -12,4 +14,7 @@ async def autoapprove(client, message: ChatJoinRequest):
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        await client.send_message(chat_id=user.id, text=TEXT.format(mention=user.mention, title=chat.title))
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
